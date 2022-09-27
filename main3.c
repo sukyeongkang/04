33f52c50a414ba9d737cbe730cf4a8dd878b1d44
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-09-27 16:47:57 +0900

    실습3

---
+++ main3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	
+	printf("input the second : ");
+	scanf("%i",&x);
+	
+	printf("%i second is %i : %i", x, x/60, x%60);
+	
+	return 0;
+}
