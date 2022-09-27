c9d8da101c12dfbe25970e5069b31a80faf0a981
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-09-27 17:08:35 +0900

    실습4

---
+++ main4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	
+	printf("input the year : ");
+	scanf("%i",&x);
+	
+	if (x%4==0 &&
+	    x%100!=0 ||
+	    x%100==0)
+	  printf("%i is the leap year\n",x);
+	else
+	  printf("%i is not leap year\n",x);
+	
+	return 0;
+}
