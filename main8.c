9d45d27024ef897f1a454eaac6ce7d00cbd6fef6
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-09-27 17:23:38 +0900

    실습6

---
+++ main8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	
+	printf("input the second : ");
+	scanf("%i",&x);
+
+	printf("The time for second is %i : %i : %i", x/3600, x%3600/60, x%3600%60);
+
+
+	return 0;
+}
